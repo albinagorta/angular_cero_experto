ddaf7fa50bade2c62e94ec41d8fd6ba292e4eb63
Angel Albinagorta <angel.albinagorta@waytech.pe>
2022-10-05 21:57:06 -0500

    pipes init

--- pipesApp/README.md
+++ pipesApp/README.md
@@ -1,4 +1,4 @@
-# PaisesApp
+# PipesApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
--- pipesApp/karma.conf.js
+++ pipesApp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/paises-app'),
+      dir: require('path').join(__dirname, './coverage/pipes-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
